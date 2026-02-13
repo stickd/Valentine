1c03de3136390faac86e0793a787110c25726c7e
Danylo Nikulin <nikulin.danilo@gmail.com>
2026-02-14 00:02:43 +0100

    Deploy Fix6

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,11 @@
 "use client";
 
-import { useState } from "react";
 import HeartGame from "../components/HeartGame";
 
 export default function Home() {
-  const [caught, setCaught] = useState(false);
-
   return (
     <div>
-      <HeartGame onCaught={() => setCaught(true)} />
-      {caught && (
-        <div className="text-center mt-10 text-2xl text-pink-700">
-          🎉 Ти спіймала серце! ❤️
-        </div>
-      )}
+      <HeartGame />
     </div>
   );
 }
